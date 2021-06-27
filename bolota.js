4c0451816719109e8cc187529618c42f81379b77
Felipe Fronchetti <fronchettl@vcu.edu>
2021-06-27 18:54:31 -0400

    Add files via upload

---
+++ bolota.js
@@ -0,0 +1,25 @@
+// Importando a biblioteca discord.js
+const Discord = require("discord.js")
+// Criando cliente no discord
+const bolotinha = new Discord.Client()
+
+// Quando o nosso cliente estiver ativo:
+bolotinha.on("ready", () => {
+  // Enviar mensagem abaixo:
+  console.log(`E nasceu das cinzas, o ${bolotinha.user.tag}!`)
+})
+
+// Quando o usuario enviar uma mensagem:
+bolotinha.on("message", message => {
+  if (message.content === "+bencao") {
+      message.reply("Bolotinha te abencoe!");
+  }
+
+  // Se o conteudo da mensagem for "ping":
+  if (message.content === "ping") { 
+    // Responder com "pong"
+    message.reply("pong");
+  }
+})
+
+bolotinha.login("Colar token de acesso")
